87ec64cd04bced77d6490eec2969bf678381570c
zhanglijun <zhanglijun@jh01>
2026-02-02 17:31:38 +0800

    更新跳转链接.

--- .vuepress/redirect.ts
+++ .vuepress/redirect.ts
@@ -45,32 +45,32 @@ export default {
         "/topics/AstroMaster/Follow":                        "/5.专业使用指南/3-轨道机动规划工具/6-跟随段.html",
 
         // ATK对象
-        "/topics/Objects/Satellite/index":                    "/4.基础使用指南/4.2创建对象.html#卫星的属性设置",     //卫星-属性；右下角的“help”按钮 
-        "/topics/Objects/Satellite/Orbit":                    "/4.基础使用指南/4.2创建对象.html#轨道属性",          //卫星-属性-点击轨道后；右下角的“help”按钮 
-        "/topics/Objects/Satellite/Attitude":                 "/4.基础使用指南/4.2创建对象.html#姿态属性",          //卫星-属性-点击姿态后；右下角的“help”按钮 
-        "/topics/Objects/Satellite/Mass":                     "/4.基础使用指南/4.2创建对象.html#质量属性",          //卫星-属性-点击质量后；右下角的“help”按钮 
-        "/topics/Objects/Satellite/PassBreak":                "/4.基础使用指南/4.2创建对象.html#轨道圈数属性",       //卫星-属性-点击轨道圈数后；右下角的“help”按钮 
-        "/topics/Objects/Satellite/2DGraphics":               "/4.基础使用指南/4.2创建对象.html#二维视图",          //卫星-属性-点击二维视图及其下属后；右下角的“help”按钮 
-        "/topics/Objects/Satellite/3DGraphics":               "/4.基础使用指南/4.2创建对象.html#三维视图",          //卫星-属性-点击三维视图及其下属后；右下角的“help”按钮 
-        "/topics/Objects/Satellite/Constraints":              "/4.基础使用指南/4.2创建对象.html#约束",             //卫星-属性-点击约束后；右下角的“help”按钮 
+        "/topics/Objects/Satellite/index":                    "/4.基础使用指南/2-创建对象.html#卫星的属性设置",     //卫星-属性；右下角的“help”按钮 
+        "/topics/Objects/Satellite/Orbit":                    "/4.基础使用指南/2-创建对象.html#轨道属性",          //卫星-属性-点击轨道后；右下角的“help”按钮 
+        "/topics/Objects/Satellite/Attitude":                 "/4.基础使用指南/2-创建对象.html#姿态属性",          //卫星-属性-点击姿态后；右下角的“help”按钮 
+        "/topics/Objects/Satellite/Mass":                     "/4.基础使用指南/2-创建对象.html#质量属性",          //卫星-属性-点击质量后；右下角的“help”按钮 
+        "/topics/Objects/Satellite/PassBreak":                "/4.基础使用指南/2-创建对象.html#轨道圈数属性",       //卫星-属性-点击轨道圈数后；右下角的“help”按钮 
+        "/topics/Objects/Satellite/2DGraphics":               "/4.基础使用指南/2-创建对象.html#二维视图",          //卫星-属性-点击二维视图及其下属后；右下角的“help”按钮 
+        "/topics/Objects/Satellite/3DGraphics":               "/4.基础使用指南/2-创建对象.html#三维视图",          //卫星-属性-点击三维视图及其下属后；右下角的“help”按钮 
+        "/topics/Objects/Satellite/Constraints":              "/4.基础使用指南/2-创建对象.html#约束",             //卫星-属性-点击约束后；右下角的“help”按钮 
         "/topics/Objects/Satellite/OrbitPlanning":            "/3.案例教程/3.4轨道机动规划工具案例/3.4.1霍曼转移.html",     //卫星属性-轨道-轨道预报器：机动规划；右下角的“help”按钮 
-        "/topics/Objects/Satellite/OrbitPlanningRPO":         "/3.案例教程/3.12RPO案例.html",                    //卫星属性-轨道-轨道预报器-机动规划-选中RPO后；右下角的“help”按钮 
-        "/topics/Objects/Satellite/AstroUq":                  "/3.案例教程/3.13偏差分析案例.html",                 //卫星属性-轨道-轨道预报器-偏差分析；右下角的“help”按钮
-        "/topics/Objects/Facility":                           "/4.基础使用指南/4.2创建对象.html#地面站的属性设置",      //地面站-属性；右下角的“help”按钮 
-        "/topics/Objects/Sensor":                             "/4.基础使用指南/4.2创建对象.html#敏感器的属性设置",      //敏感器-属性；右下角的“help”按钮 
-        "/topics/Objects/Receiver":                             "/4.基础使用指南/4.2创建对象.html#接收器的属性设置",      //接收器-属性；右下角的“help”按钮 
-        "/topics/Objects/Transmitter":                             "/4.基础使用指南/4.2创建对象.html#发射器的属性设置",      //发射器-属性；右下角的“help”按钮 
-        "/topics/Objects/SatelliteCollection":                "/3.案例教程/3.9巨型星座设计案例.html",             //卫星集群-属性：右下角的“help”按钮 
-        "/topics/Objects/CoverageDefinition":                 "/3.案例教程/3.11区域覆盖案例.html#创建任务场景与对象",     //覆盖定义-属性：右下角的“help”按钮 
+        "/topics/Objects/Satellite/OrbitPlanningRPO":         "/3.案例教程/12-RPO案例.html",                    //卫星属性-轨道-轨道预报器-机动规划-选中RPO后；右下角的“help”按钮 
+        "/topics/Objects/Satellite/AstroUq":                  "/3.案例教程/13-偏差分析案例.html",                 //卫星属性-轨道-轨道预报器-偏差分析；右下角的“help”按钮
+        "/topics/Objects/Facility":                           "/4.基础使用指南/2-创建对象.html#地面站的属性设置",      //地面站-属性；右下角的“help”按钮 
+        "/topics/Objects/Sensor":                             "/4.基础使用指南/2-创建对象.html#敏感器的属性设置",      //敏感器-属性；右下角的“help”按钮 
+        "/topics/Objects/Receiver":                             "/4.基础使用指南/2-创建对象.html#接收器的属性设置",      //接收器-属性；右下角的“help”按钮 
+        "/topics/Objects/Transmitter":                             "/4.基础使用指南/2-创建对象.html#发射器的属性设置",      //发射器-属性；右下角的“help”按钮 
+        "/topics/Objects/SatelliteCollection":                "/3.案例教程/9-巨型星座设计案例.html",             //卫星集群-属性：右下角的“help”按钮 
+        "/topics/Objects/CoverageDefinition":                 "/3.案例教程/11-区域覆盖案例.html#创建任务场景与对象",     //覆盖定义-属性：右下角的“help”按钮 
 
-        "/topics/Scenario/CoverageDefinition":                 "/4.基础使用指南/4.1创建场景.html#设置场景属性",     //场景-属性：右下角的“help”按钮
+        "/topics/Scenario/CoverageDefinition":                 "/4.基础使用指南/1-创建场景.html#设置场景属性",     //场景-属性：右下角的“help”按钮
 
         "/topics/Intergrating/Client":                          "/二次开发教程/4-ATK客户端.md",
 
         "/topics/Release/ChangeLog":                            "/发布说明/最新版发布说明.md",
         
-        "/topics/senario/create":       "/4.基础使用指南/4.1创建场景.html#xxx",     // 
-        "/topics/senario/xxx":          "/4.基础使用指南/4.1创建场景.html#xxxx",     // 
+        "/topics/senario/create":       "/4.基础使用指南/1-创建场景.html#xxx",     // 
+        "/topics/senario/xxx":          "/4.基础使用指南/1-创建场景.html#xxxx",     // 
 
         "/topics/Attitude/AttitudeFixedInAxes":                 "/4.基础使用指南/基础模型/2-姿态定义/1-固定姿态.md",
         "/topics/Attitude/AttitudeFixedInCBF":                  "/4.基础使用指南/基础模型/2-姿态定义/2-固定于天体固定系姿态.md",
